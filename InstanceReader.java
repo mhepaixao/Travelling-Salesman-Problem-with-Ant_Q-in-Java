cec8bca78e16f3ebcf8b0f43b36b1cae2c65bedc
Matheus Paixao <mhepaixao@gmail.com>
2012-05-21 17:21:44 -0300

    Reading more formats of cartesian type instances.

--- InstanceReader.java
+++ InstanceReader.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.ArrayList;
 
 /**
@@ -14,6 +17,7 @@ import java.util.ArrayList;
  * @author: Matheus Paixão 
  */
 public class InstanceReader extends JFrame {
+   Pattern pattern;
 
    /**
     * Method to get the list of nodes.
@@ -42,6 +46,7 @@ public class InstanceReader extends JFrame {
       if(instance != null){
          try{
             String instanceLine = null;
+            String[] values;
             City city = null;
             double x = 0;
             double y = 0;
@@ -50,13 +55,44 @@ public class InstanceReader extends JFrame {
             while(reader.ready()){
                instanceLine = reader.readLine();
 
-               //instance formart 1:
-               x = Double.parseDouble(instanceLine.split(" ")[0]);
-               y = Double.parseDouble(instanceLine.split(" ")[1]);
+               pattern = Pattern.compile("[a-z][a-z]");
+               Matcher twoLettersMatcher = pattern.matcher(instanceLine) ;
+               pattern = Pattern.compile("[0-9]");
+               Matcher numbersMatcher = pattern.matcher(instanceLine) ;
 
-               city = new City(x, y);
+               if((twoLettersMatcher.find() == false) && (numbersMatcher.find() == true)){
+                  pattern = Pattern.compile("\\s{2,}");
+                  Matcher spacesMatcher = pattern.matcher(instanceLine);
+                  instanceLine = spacesMatcher.replaceAll(" ").trim();
 
-               dynamicListOfCities.add(city);
+                  values = instanceLine.split(" ");
+                  if(values.length > 3){
+                     //distance matrix format
+                  }
+                  else{
+                     if(values.length == 3){
+                        if(instanceLine.contains("e")){
+                           city = getExpCartesianCity(values[1], values[2]);
+                        }
+                        else{
+                           city = getCartesianCity(values[1], values[2]);
+                        }
+                     }
+                     else{
+                        if(instanceLine.contains("e")){
+                           city = getExpCartesianCity(values[0], values[1]);
+                        }
+                        else{
+                           city = getCartesianCity(values[0], values[1]);
+                        }
+                     }
+                  }
+
+               }
+               
+               if(city != null){
+                  dynamicListOfCities.add(city);
+               }
             }
          }
          catch(Exception e){
@@ -91,5 +127,24 @@ public class InstanceReader extends JFrame {
       }
 
       return instance;
+   }
+
+   private City getExpCartesianCity(String value1, String value2){
+      String[]  xValues = value1.split("e+");
+      double x = Double.parseDouble(xValues[0]);
+      double xPower = Double.parseDouble(xValues[1]);
+
+      String[]  yValues = value2.split("e+");
+      double y = Double.parseDouble(yValues[0]);
+      double yPower = Double.parseDouble(yValues[1]);
+
+      return new City(x * Math.pow(10, xPower), y * Math.pow(10, yPower));
+   }
+
+   private City getCartesianCity(String value1, String value2){
+      double x = Double.parseDouble(value1);
+      double y = Double.parseDouble(value2);
+
+      return new City(x, y);
    }
 }
